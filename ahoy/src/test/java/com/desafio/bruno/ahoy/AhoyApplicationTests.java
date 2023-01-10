0f897c93e4c09c00af39a47b8f02cb3c897852d7
bruno.silveira <bruno.silveira@spcm.com.br>
2023-01-10 12:28:03 -0300

    inciando o projeto

---
+++ ahoy/src/test/java/com/desafio/bruno/ahoy/AhoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.bruno.ahoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
